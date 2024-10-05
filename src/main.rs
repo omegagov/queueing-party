1600a754e1c2d51d15ecf5a8a11283a9c81bcc0e
Robert Scott <code@humanleg.org.uk>
2024-10-05 17:39:32 +0100

    add basic main loop implementation

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::rc::Rc;
 use std::hash::{Hash, Hasher};
 use std::cmp::{Eq, PartialEq, Ordering, max};
 
-use rand::Rng;
+use rand::{Rng, SeedableRng};
 use rand::seq::SliceRandom;
 use rand_distr::{Distribution, LogNormal};
 use rand_xoshiro::Xoshiro256StarStar;
@@ -327,6 +327,79 @@ struct WorkerToken {
     originating_queue_name: String,
 }
 
+impl WorkerToken {
+    fn mk_token_restoring_handler<A, Ri, Ro>(
+        inner_handler: impl FnOnce(A) -> Ri,
+    ) -> impl FnOnce(A) -> Ro
+    where
+        A: HasTimestamp,
+        Ri: HasProposedEvents + HasWorkerTokensToRestore + IntoLossy<Ro>,
+        Ro: HasProposedEvents,
+    {
+        |args: A| {
+            let timestamp = *args.get_timestamp();
+            // call inner handler
+            let mut ret = inner_handler(args);
+
+            // proposed events that follow-on handlers may produce
+            let mut followon_proposed_events = Vec::new();
+
+            // handle restored tokens
+            for mut token in ret.get_worker_tokens_to_restore_mut().drain(..) {
+                assert!(
+                    token.checkout_timestamp < timestamp,
+                    "Cannot restore WorkerToken until after time period it was checked out",
+                );
+
+                let nonempty_queues = Vec::from_iter(
+                    token.worker.subscribed_queues.iter().filter(|q| !q.borrow().deque.is_empty())
+                );
+                if nonempty_queues.is_empty() {
+                    // return worker to all subscribed queues
+                    let worker_rc = Rc::new(token.worker);
+                    for queue in &worker_rc.subscribed_queues {
+                        queue.borrow_mut().listening_workers.insert(worker_rc.clone());
+                    }
+                } else {
+                    // this worker picks up a new handler from a nonempty queue
+
+                    // choose a nonempty queue
+                    let chosen_queue = SliceRandom::choose(
+                        &nonempty_queues[..],
+                        &mut token.worker.rng,
+                    ).unwrap();
+                    let followon_handler = chosen_queue.borrow_mut().deque.pop_front().unwrap();
+                    let chosen_queue_name = chosen_queue.borrow().name.clone();
+
+                    // prepare args
+                    let followon_args = BasicArgs {
+                        timestamp: timestamp,
+                        worker_token: Some(WorkerToken {
+                            originating_queue_name: chosen_queue_name,
+                            worker: token.worker,
+                            checkout_timestamp: timestamp,
+                        }),
+                        ..Default::default()
+                    };
+
+                    // TODO tally metric
+
+                    // call follow-on handler
+                    let mut followon_ret = followon_handler(followon_args);
+
+                    // gather its proposed events
+                    followon_proposed_events.append(followon_ret.get_proposed_events_mut());
+                }
+            }
+
+            // combine proposed events from follow-ons into our ret
+            ret.get_proposed_events_mut().append(&mut followon_proposed_events);
+
+            return ret.into_lossy();
+        }
+    }
+}
+
 trait HasWorkerToken {
     fn get_worker_token(&self) -> &Option<WorkerToken>;
     fn get_worker_token_mut(&mut self) -> &mut Option<WorkerToken>;
@@ -450,83 +523,68 @@ struct Autoscaler {
     //  metrics: ...
 }
 
-impl WorkerToken {
-    fn mk_token_restoring_handler<A, Ri, Ro>(
-        inner_handler: impl FnOnce(A) -> Ri,
-    ) -> impl FnOnce(A) -> Ro
-    where
-        A: HasTimestamp,
-        Ri: HasProposedEvents + HasWorkerTokensToRestore + IntoLossy<Ro>,
-        Ro: HasProposedEvents,
-    {
-        |args: A| {
-            let timestamp = *args.get_timestamp();
-            // call inner handler
-            let mut ret = inner_handler(args);
-
-            // proposed events that follow-on handlers may produce
-            let mut followon_proposed_events = Vec::new();
-
-            // handle restored tokens
-            for mut token in ret.get_worker_tokens_to_restore_mut().drain(..) {
-                assert!(
-                    token.checkout_timestamp < timestamp,
-                    "Cannot restore WorkerToken until after time period it was checked out",
-                );
-
-                let nonempty_queues = Vec::from_iter(
-                    token.worker.subscribed_queues.iter().filter(|q| !q.borrow().deque.is_empty())
-                );
-                if nonempty_queues.is_empty() {
-                    // return worker to all subscribed queues
-                    let worker_rc = Rc::new(token.worker);
-                    for queue in &worker_rc.subscribed_queues {
-                        queue.borrow_mut().listening_workers.insert(worker_rc.clone());
-                    }
-                } else {
-                    // this worker picks up a new handler from a nonempty queue
-
-                    // choose a nonempty queue
-                    let chosen_queue = SliceRandom::choose(
-                        &nonempty_queues[..],
-                        &mut token.worker.rng,
-                    ).unwrap();
-                    let followon_handler = chosen_queue.borrow_mut().deque.pop_front().unwrap();
-                    let chosen_queue_name = chosen_queue.borrow().name.clone();
-
-                    // prepare args
-                    let followon_args = BasicArgs {
-                        timestamp: timestamp,
-                        worker_token: Some(WorkerToken {
-                            originating_queue_name: chosen_queue_name,
-                            worker: token.worker,
-                            checkout_timestamp: timestamp,
-                        }),
-                        ..Default::default()
-                    };
-
-                    // TODO tally metric
-
-                    // call follow-on handler
-                    let mut followon_ret = followon_handler(followon_args);
-
-                    // gather its proposed events
-                    followon_proposed_events.append(followon_ret.get_proposed_events_mut());
-                }
-            }
-
-            // combine proposed events from follow-ons into our ret
-            ret.get_proposed_events_mut().append(&mut followon_proposed_events);
-
-            return ret.into_lossy();
-        }
-    }
-}
-
 // worker_token.worker.metrics. ...
 
 // ... "add worker" routine?
 
+struct ScheduledEvent {
+    due_time: u64,
+    handler: Box<dyn FnOnce(u64) -> Vec<ProposedEvent>>,
+}
+
+impl Ord for ScheduledEvent {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.due_time.cmp(&other.due_time).reverse()
+    }
+}
+
+impl PartialOrd for ScheduledEvent {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for ScheduledEvent {
+    fn eq(&self, other: &Self) -> bool {
+        self.due_time == other.due_time
+    }
+}
+
+impl Eq for ScheduledEvent {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut event_heap: BinaryHeap<ScheduledEvent> = Default::default();
+    let mut rng = Xoshiro256StarStar::seed_from_u64(0);
+
+    // ...
+    while !event_heap.is_empty() {
+        let mut simultaneous_events: Vec<ScheduledEvent> = Default::default();
+        while let Some(event) = event_heap.peek() {
+            if let Some(ScheduledEvent {due_time: existing_time, ..}) = simultaneous_events.first() {
+                if *existing_time == event.due_time {
+                    break;
+                }
+            }
+            simultaneous_events.push(event_heap.pop().unwrap());
+        }
+
+        SliceRandom::shuffle(
+            &mut simultaneous_events[..],
+            &mut rng,
+        );
+        let mut proposed_events: Vec<ProposedEvent> = simultaneous_events.drain(..).flat_map(|event| {
+            (event.handler)(event.due_time)
+        }).collect();
+
+        // TODO more efficient bulk implementation
+        for proposed_event in proposed_events.drain(..) {
+            event_heap.push(ScheduledEvent {
+                due_time: max(
+                    1,
+                    proposed_event.due_time.sample(&mut rng) as u64,
+                ),
+                handler: proposed_event.handler,
+            });
+        }
+    }
 }
